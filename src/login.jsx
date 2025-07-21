64f1df39f86bac359acd7277f174147553cc2f04
christ20351 <teneledou@gmail.com>
2025-07-21 20:14:11 +0000

    Votre message de commit

--- src/login.jsx
+++ src/login.jsx
@@ -6,7 +6,7 @@ function Login() {
   return (
    <div className="flex flex-row ">
    <div className="w-[50%] h-screen p-24  ">
-     <p className="flex flex-row gap-2 font-bold text-black"> <div className="w-5 h-5 bg-purple-500 rounded-full"></div>Finnger</p>
+     <p className="flex flex-row gap-2 font-bold text-black ali"> <div className="w-5 h-5 bg-purple-500 rounded-full"></div>Finnger</p>
    <div className="mt-20">
      <h1 className="mt-5 text-5xl font-bold">Holla,</h1>
     <h1 className="mt-3 text-5xl font-bold ">Welcome Back</h1>
@@ -16,20 +16,19 @@ function Login() {
       <input type="password"  className="h-10 p-2 border border-gray-400 rounded-md"/>
     <div className="flex flex-row justify-between">
        <div className="flex flex-row gap-2">
-         <input type="checkbox" name="" id="" className="accent-purple-600 checked:bg-purple-500" checked />
+         <input type="checkbox" name="" id="" className="accent-purple-600 checked:bg-purple-500"  />
         <p className="text-gray-500">remember me</p>
        </div>
        <p className="text-gray-500">forget Password</p>
     </div>
-      <Link
-        to="/signup"
+      <button
         className="font-bold text-center bg-purple-600 w-[130px] h-[50px] rounded-md text-white mt-10 flex items-center justify-center"
       >
         Sign in
-      </Link>
+      </button>
     </form>
    </div>
-   <p className="mt-20 text-gray-500">Don't have an account? <span className="font-bold text-purple-600">Sign up</span></p>
+   <p className="mt-20 text-gray-500">Don't have an account? <Link  to="/signup" className="font-bold text-purple-600">Sign up</Link></p>
    </div>
    <div className="w-[50%] h-screen  flex items-center justify-center">
      <img
